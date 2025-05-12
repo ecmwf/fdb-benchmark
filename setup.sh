273199b2872df441f8e4f35737c24dd5b94fb03e
Nicolau Manubens <nicolau.manubens@ecmwf.int>
2025-05-12 20:41:57 +0100

    Improved setup script.

--- setup.sh
+++ setup.sh
@@ -2,18 +2,132 @@
 
 set -e
 
-# TODO: arguments
 
-# root
-root=/storage/nx01/nx01/manm/fdb-hammer-parallel
 
-# rebuild or not
-rebuild=true
-# with profiling or not
-# with local lustre
-#   lustre root path
-# with remote fdb
-# prolog script to run on all nodes (e.g. module loads)
+# --- parse arguments
+
+root=/tmp/fdb-hammer-parallel
+rebuild="false"
+backend=
+fdb_root=
+fdb_root_pool=
+fdb_remote_endpoint=
+
+POSITIONAL=()
+while [[ $# -gt 0 ]] ; do
+key="$1"
+case $key in
+    -h|--help)
+    echo -e "\
+Usage:\n\n\
+./setup.sh [options]\n\n\
+Available options:\n\n\
+--root <path>\n\nPath to a root directory where to build and install FDB and other repositories. Defaults to /tmp/fdb-hammer-parallel.\n\n\
+--rebuild\n\nFlag to trigger rebuild from scratch rather than reuse previously built binaries.\n\n\
+--backend <backend>\n\nName of the storage backend the benchmark will be run against. Can be posix, lustre, nfs, remote, daos, or ceph. Has no default.\n\n\
+--fdb-root <path>\n\nPath to an existing root directory for the FDB posix, lustre, or nfs backend to internally store its data and indices.\n\n\
+--fdb-root-pool <pool>\n\nName of an existing root pool for the FDB daos or ceph backend to internally store its data and indices.\n\n\
+--fdb-remote-endpoint <host:port>\n\nHost and port where the remote FDB server is running.\n\n\
+-h|--help\n\nshow this menu\
+"
+    exit 0
+    ;;
+    --root)
+    root="$2"
+    shift
+    shift
+    ;;
+    --rebuild)
+    rebuild="true"
+    shift
+    ;;
+    --backend)
+    backend="$2"
+    shift
+    shift
+    ;;
+    --fdb-root)
+    fdb_root="$2"
+    shift
+    shift
+    ;;
+    --fdb-root-pool)
+    fdb_root_pool="$2"
+    shift
+    shift
+    ;;
+    --fdb-remote-endpoint)
+    fdb_remote_endpoint="$2"
+    shift
+    shift
+    ;;
+    *)
+    POSITIONAL+=( "$1" )
+    shift
+    ;;
+esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+if [ ${#POSITIONAL[@]} -ne 0 ] ; then
+    echo "No positional arguments were expected. Check ./setup.sh --help."
+    exit 1
+fi
+
+
+
+# --- check backend and set build flags
+
+build_flags=""
+
+[[ -z "$backend" ]] && echo "A --backend must be specified." && exit 1
+
+case $backend in
+
+  posix)
+  build_flags="-DENABLE_LUSTRE=OFF"
+  ;;
+
+  lustre)
+  build_flags="-DENABLE_LUSTRE=ON"
+  ;;
+
+  nfs)
+  build_flags="-DENABLE_NFS=ON"
+  echo "fdb-hammer on NFS not available" && exit 1
+  ;;
+
+  remote)
+  build_flags="-DENABLE_REMOTE_FDB=ON"
+  ;;
+
+  daos)
+  build_flags="-DENABLE_DAOSFDB=ON"
+  ;;
+
+  ceph)
+  build_flags="-DENABLE_RADOSFDB=ON"
+  echo "fdb-hammer on Ceph not available" && exit 1
+  ;;
+
+  *)
+  echo "backend not recognised" && exit 1
+  ;;
+
+esac
+
+if [[ "$backend" == "posix" ]] || [[ "$backend" == "lustre" ]] || [[ "$backend" == "nfs" ]] ; then
+  [[ -z "$fdb_root" ]] && echo "Must provide an --fdb-root directory if ---backend is ${backend}." && exit 1
+fi
+
+if [[ "$backend" == "daos" ]] || [[ "$backend" == "ceph" ]] ; then
+  [[ -z "$fdb_root_pool" ]] && echo "Must provide an --fdb-root-pool if --backend is ${backend}." && exit 1
+fi
+
+if [[ "$backend" == "remote" ]] ; then
+  [[ -z "$fdb_remote_endpoint" ]] && echo "Must provide an --fdb-remote-endpoint if --backend is ${backend}." && exit 1
+fi
 
 
 
@@ -83,35 +197,7 @@ if [ ! -e $fdb_dir ] || [[ "$rebuild" == "true" ]] ; then
   mkdir -p $fdb_build_dir
   cd $fdb_build_dir
   export PATH=$PATH:${git_dir}/ecbuild/bin
-  cmake ${git_dir}/fdb-bundle -DENABLE_MEMFS=ON -DENABLE_AEC=OFF
-
-  # --- only if enabled profiling
-
-  #cd ${git_dir}/fdb-bundle/fdb5/src/fdb5/tools
-  #wget -O fdb-hammer.cc https://raw.githubusercontent.com/ecmwf/fdb/refs/heads/manm_fdbhammer_prof/src/fdb5/tools/fdb-hammer.cc
-
-  #cd ${git_dir}/fdb-bundle/fdb5
-
-  # TODO: in src/fdb5/tools/fdb-hammer.cc, for every instance of
-  #
-  # // uncomment for rados runs
-  #
-  # comment out the line following it
-
-  # TODO: in src/fdb5/message/MessageArchiver.cc::archive, comment out the following lines
-  #
-  # eckit::Timer timer("fdb::service::archive");
-  #
-  # eckit::Progress progress("FDB archive", 0, source.estimate());
-  # 
-  # progress(total_size);
-  # 
-  # eckit::Log::info() << "FDB archive " << eckit::Plural(count, "message") << ","
-  #                    << " size " << eckit::Bytes(total_size) << ","
-  #                    << " in " << eckit::Seconds(timer.elapsed()) << " (" << eckit::Bytes(total_size, timer) << ")"
-  #                    << std::endl;
-
-  #cd $fdb_build_dir
+  cmake ${git_dir}/fdb-bundle -DENABLE_MEMFS=ON -DENABLE_AEC=OFF ${build_flags}
 
   cmake --build . -j 12
   mkdir -p $fdb_dir
@@ -129,21 +215,72 @@ fi
 
 # --- FDB client config
 
-cat > ${root}/config.yaml.in <<EOF
+if [[ "$backend" == "posix" ]] || [[ "$backend" == "lustre" ]] || [[ "$backend" == "nfs" ]] ; then
+
+  cat > ${root}/config.yaml.in <<EOF
 type: local
 spaces:
 - roots:
-  - path: /storage/nx01/nx01/manm/itt_debug_fdb_root
+  - path: ${fdb_root}
 schema: @SCHEMA_PATH@
 engine: toc
 store: file
 useSubToc: true
-
-#type: remote
-#host: hostname_remote_catalogue
-#port: 10000
-#engine: remote
-#store: remote
 EOF
+
+fi
+
+
+if [[ "$backend" == "daos" ]] ; then
+
+  cat > ${root}/config.yaml.in <<EOF
+type: local
+schema: @SCHEMA_PATH@
+engine: daos
+store: daos
+daos:
+  catalogue:
+    pool: ${root_pool}
+    root_cont: root_container
+  store:
+    pool: ${root_pool}
+  client:
+    container_oids_per_alloc: 10000
+EOF
+
+fi
+
+
+if [[ "$backend" == "ceph" ]] ; then
+
+  cat > ${root}/config.yaml.in <<EOF
+type: local
+schema: @SCHEMA_PATH@
+engine: rados
+store: rados
+rados:
+  pool: ${root_pool}
+  store:
+    maxHandleBuffSize: 100000
+EOF
+
+fi
+
+
+if [[ "$backend" == "remote" ]] ; then
+
+  fdb_remote_host=${fdb_remote_endpoint%:*}
+  fdb_remote_port=${fdb_remote_endpoint#*:}
+
+  cat > ${root}/config.yaml.in <<EOF
+type: remote
+host: $fdb_remote_host
+port: $fdb_remote_port
+engine: remote
+store: remote
+EOF
+
+fi
+
 
 cd $cwd

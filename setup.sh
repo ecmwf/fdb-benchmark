3109803adc4ea411baf809d88d19ca228354350a
Simon Smart <simon.smart@ecmwf.int>
2025-10-27 00:02:06 +0100

    WIP refactoring

--- setup.sh
+++ setup.sh
@@ -184,10 +184,10 @@ endif()
 ecbuild_bundle_initialize()
 
 ecbuild_bundle( PROJECT eccodes         GIT "https://github.com/ecmwf/eccodes"               TAG 2.43.0   UPDATE)
-ecbuild_bundle( PROJECT eckit           GIT "https://github.com/ecmwf/eckit"                 TAG 1.32.2   UPDATE)
+ecbuild_bundle( PROJECT eckit           GIT "https://github.com/ecmwf/eckit"                 TAG 83dfbaaa447d4457cd4d2561def3e670102c13ae )
 ecbuild_bundle( PROJECT odc             GIT "https://github.com/ecmwf/odc"                   TAG 1.6.1   UPDATE)
 ecbuild_bundle( PROJECT metkit          GIT "https://github.com/ecmwf/metkit"                TAG 1.15.2   UPDATE)
-ecbuild_bundle( PROJECT fdb5            GIT "https://github.com/ecmwf/fdb"                   TAG 6afe3da7c66ea163ca177e58f8d4b1edcb38f96d UPDATE)
+ecbuild_bundle( PROJECT fdb5            GIT "https://github.com/ecmwf/fdb"                   TAG 10c64f37ab954f1e8245308c5f7476640e2bbba6 )
 
 ecbuild_bundle_finalize()
 EOF
